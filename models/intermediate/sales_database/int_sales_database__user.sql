98586e781136a16fd6f619cd208ec619c36c7aaa
dyhia REHOUNE <dihia.rahoun94@yahoo.fr>
2026-07-08 08:42:16 +0000

    feat: debug int_sales_database__user

--- models/intermediate/sales_database/int_sales_database__user.sql
+++ models/intermediate/sales_database/int_sales_database__user.sql
@@ -2,30 +2,29 @@ WITH
   orders_summary AS (
     SELECT
       user_id,
+      user_city,
+      user_state,
       SUM(total_order_amount) AS total_amount_spent,
       SUM(total_items) AS total_items,
       SUM(total_distinct_items) AS total_distinct_items,
       COUNT(DISTINCT order_id) AS total_orders
     FROM
       {{ ref('int_sales_database__order') }}
-    GROUP BY user_id
+    GROUP BY user_id, user_city, user_state
   )
 
 SELECT
-  o.user_id,
-  o.user_city,
-  o.user_state,
+  os.user_id,
+  os.user_city,
+  os.user_state,
   os.total_amount_spent,
   os.total_items,
   os.total_distinct_items,
   os.total_orders,
   p.favorite_product_id
-FROM {{ ref('int_sales_database__order') }} AS o
-INNER JOIN 
-  orders_summary AS os
-  ON os.user_id = o.user_id
+FROM orders_summary AS os
 LEFT JOIN
   {{ ref('int_sales_database__user_favorite_product') }} AS p
   ON
-    o.user_id = p.user_id
+    os.user_id = p.user_id
 
